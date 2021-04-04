e8b4a2e64c9aaf236de77592bd6540966f90de8b
madsq98 <mads.q@outlook.com>
2021-04-04 14:51:34 +0200

    DAL work

--- src/BLL/AccountBLL.java
+++ src/BLL/AccountBLL.java
@@ -1,4 +1,34 @@
 package BLL;
 
+import BE.Account;
+import DAL.Server.AccountDAL;
+
+import java.sql.SQLException;
+
 public class AccountBLL {
+    private AccountDAL dbAccess;
+
+    public AccountBLL() {
+        dbAccess = new AccountDAL();
+    }
+
+    public Account checkLogin(String username, String password) throws SQLException {
+        if(!username.isEmpty() && !password.isEmpty()) {
+            Account check = dbAccess.getAccountByUsername(username);
+
+            return (check.getPassword().equals(password)) ? check : null;
+        } else
+            return null;
+    }
+
+    public boolean newAccount(Account a) throws SQLException {
+        int newId = dbAccess.saveNewAccount(a);
+
+        if(newId == -1)
+            return false;
+
+        a.setId(newId);
+
+        return true;
+    }
 }
--- src/DAL/AccountDAL.java
+++ src/DAL/AccountDAL.java
@@ -4,10 +4,7 @@ import BE.Account;
 import BE.UserType;
 
 import javax.xml.transform.Result;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class AccountDAL {
     private Connection conn;
@@ -16,12 +13,11 @@ public class AccountDAL {
         conn = MSSQLHandler.getConnection();
     }
 
-    public Account getAccountLogin(String username, String password) throws SQLException {
-        String query = "SELECT Accounts.id, Accounts.username, Accounts.type, UserInfo.firstName, UserInfo.lastName, UserInfo.email, UserInfo.phone FROM Accounts INNER JOIN UserInfo ON UserInfo.accountId = Accounts.id WHERE Accounts.username = ? AND Accounts.password = ?";
+    public Account getAccountByUsername(String username) throws SQLException {
+        String query = "SELECT Accounts.id, Accounts.username, Accounts.type, UserInfo.firstName, UserInfo.lastName, UserInfo.email, UserInfo.phone FROM Accounts INNER JOIN UserInfo ON UserInfo.accountId = Accounts.id WHERE Accounts.username = ?";
 
         PreparedStatement execute = conn.prepareStatement(query);
         execute.setString(1,username);
-        execute.setString(2,password);
 
         ResultSet rs = execute.executeQuery();
 
@@ -30,15 +26,52 @@ public class AccountDAL {
 
         int id = rs.getInt("id");
         String user = rs.getString("username");
+        String pass = rs.getString("password");
         UserType type = (rs.getInt("type") == 1) ? UserType.STUDENT : UserType.TEACHER;
         String firstName = rs.getString("firstName");
         String lastName = rs.getString("lastName");
         String email = rs.getString("email");
         int phone = rs.getInt("phone");
 
-        Account returnAccount = new Account(user,type,firstName,lastName,email,phone);
+        Account returnAccount = new Account(user,pass,type,firstName,lastName,email,phone);
         returnAccount.setId(id);
 
         return returnAccount;
+    }
+
+    public int saveNewAccount(Account a) throws SQLException {
+        String query = "INSERT INTO Accounts ('username','password','type') VALUES (?,?,?)";
+        PreparedStatement ps = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1,a.getUsername());
+        ps.setString(2,a.getPassword());
+        ps.setInt(3,(a.getType() == UserType.STUDENT) ? 1 : 2);
+
+        int rows = ps.executeUpdate();
+
+        if(rows == 0)
+            return -1;
+
+        ResultSet rs = ps.getGeneratedKeys();
+
+        if(rs.next()) {
+            int newId = rs.getInt(1);
+
+            String query2 = "INSERT INTO UserInfo ('accountId','firstName','lastName','email','phone') VALUES (?,?,?,?,?)";
+            PreparedStatement ps2 = conn.prepareStatement(query2);
+            ps.setInt(1,newId);
+            ps.setString(2,a.getFirstName());
+            ps.setString(3,a.getLastName());
+            ps.setString(4,a.getEmail());
+            ps.setInt(5,a.getPhone());
+
+            int rows2 = ps.executeUpdate();
+
+            if(rows2 == 0)
+                return -1;
+
+            return newId;
+        }
+        else
+            return -1;
     }
 }
