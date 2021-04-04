c759383a4434e47271d30344af9aaeb5b8752546
madsq98 <mads.q@outlook.com>
2021-04-04 16:05:46 +0200

    Update SchemaBLL.java

--- src/BLL/SchemaBLL.java
+++ src/BLL/SchemaBLL.java
@@ -5,7 +5,12 @@ import BE.Lesson;
 import DAL.SchemaDAL;
 
 import java.sql.SQLException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class SchemaBLL {
@@ -22,5 +27,40 @@ public class SchemaBLL {
 
     public List<Lesson> getAllLessons() {
         return allLessons;
+    }
+
+    public List<List<Lesson>> getWeekSchema() {
+        Calendar cal = Calendar.getInstance();
+
+        List<Lesson> monday = new ArrayList<>();
+        List<Lesson> tuesday = new ArrayList<>();
+        List<Lesson> wednesday = new ArrayList<>();
+        List<Lesson> thursday = new ArrayList<>();
+        List<Lesson> friday = new ArrayList<>();
+        for(int i = Calendar.MONDAY; i <= Calendar.FRIDAY; i++) {
+            cal.set(Calendar.DAY_OF_WEEK, i);
+            LocalDate thisDate = cal.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            for(Lesson l : allLessons) {
+                LocalDate comparison = l.getStartTime().toLocalDateTime().toLocalDate();
+                if(thisDate.equals(comparison)) {
+                    switch(i) {
+                        case Calendar.MONDAY -> monday.add(l);
+                        case Calendar.TUESDAY -> tuesday.add(l);
+                        case Calendar.WEDNESDAY -> wednesday.add(l);
+                        case Calendar.THURSDAY -> thursday.add(l);
+                        case Calendar.FRIDAY -> friday.add(l);
+                    }
+                }
+            }
+        }
+
+        List<List<Lesson>> returnLessons = new ArrayList<>();
+        returnLessons.add(monday);
+        returnLessons.add(tuesday);
+        returnLessons.add(wednesday);
+        returnLessons.add(thursday);
+        returnLessons.add(friday);
+
+        return returnLessons;
     }
 }
