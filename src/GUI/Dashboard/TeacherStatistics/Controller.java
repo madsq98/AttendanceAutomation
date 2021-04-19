d7bdddf50e48b3b37a92bd859770f76ffefc5dad
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2021-04-19 11:57:19 +0200

    test
    test

--- src/GUI/Dashboard/TeacherStatistics/Controller.java
+++ src/GUI/Dashboard/TeacherStatistics/Controller.java
@@ -1,19 +1,35 @@
 package GUI.Dashboard.TeacherStatistics;
 
 import BE.Account;
+import BE.Course;
+import BE.Lesson;
 import BLL.AccountBLL;
 import BLL.AttendanceBLL;
 import BLL.SchemaBLL;
 import GUI.Dashboard.Interfaces.ISubPage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Controller implements ISubPage {
+    private Course selectedCourse;
     private Account currentAccount;
     private AccountBLL accountBLL;
     private SchemaBLL schemaBLL;
     private AttendanceBLL attendanceBLL;
     private GUI.Dashboard.Controller mainController;
+    private ObservableList<Course> observableCourses;
+    private ObservableList<Lesson> observableLessons;
+    private ObservableList<Account> observableAccount;
+
+
+    @FXML
+    private ComboBox<Course>  courseSelector;
 
     @Override
     public void setCurrentAccount(Account a) {
@@ -28,6 +44,15 @@ public class Controller implements ISubPage {
     @Override
     public void setSchemaBLL(SchemaBLL schemaBLL) {
         this.schemaBLL = schemaBLL;
+
+        try {
+            observableCourses.addAll(schemaBLL.getUserCourses(currentAccount));
+            courseSelector.setItems(observableCourses);
+            courseSelector.getSelectionModel().selectFirst();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 
     @Override
@@ -40,6 +65,33 @@ public class Controller implements ISubPage {
 
     @Override
     public void load() {
+    }
+
+    public void initialize() {
+        observableAccount = FXCollections.observableArrayList();
+        observableCourses = FXCollections.observableArrayList();
+        observableLessons = FXCollections.observableArrayList();
+
+
+        courseSelector.valueProperty().addListener((observable, oldValue, newValue) -> {
+            selectedCourse = newValue;
+            try {
+                update();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+    }
+
+    public void update () throws SQLException {
+        if (selectedCourse != null) {
+            LocalDate firstDate = schemaBLL.getFirstLesson(selectedCourse).getStartTime().toLocalDateTime().toLocalDate();
+            LocalDate lastDate = LocalDate.now();
+            observableLessons.setAll(schemaBLL.getLessonsInterval(firstDate,lastDate,selectedCourse));
+            observableAccount.setAll(attendanceBLL.getAccountsFromCourse(selectedCourse));
+
+        }
+
 
     }
 }
